1be1aa9de406590b771d14c75bc744e8ac2108bb
JM Bell <justinmbell1997@gmail.com>
2025-05-20 21:04:48 -0500

    Initial Commit

---
+++ CryptoConverter/CryptoConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoConverterApp.swift
+//  CryptoConverter
+//
+//  Created by Justin Bell on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
